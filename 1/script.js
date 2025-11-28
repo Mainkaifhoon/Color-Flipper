6246238ef2b613efb7361914c44f74a797351fcb
mainkaifhoon <pnmkaif.gmail.com>
2025-11-28 23:40:24 +0530

    Initial commit: Color Flipper project

---
+++ 1/script.js
@@ -0,0 +1,12 @@
+const body = document.getElementsByTagName('body')[0]
+
+function setColor(name){
+    body.style.backgroundColor=name;
+}
+
+function randomColor(){
+    const red = Math.round(Math.random() *255)
+    const green = Math.round(Math.random() *255)
+    const blue = Math.round(Math.random() *255)
+    body.style.backgroundColor= `rgb(${red}, ${green}, ${blue})`;
+}
